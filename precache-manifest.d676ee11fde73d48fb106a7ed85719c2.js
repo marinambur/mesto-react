5f7fcf9a8a0bcef1693ce0385d8af6fd72dad5ae
Marina Kuznetsova <marinaburykina@gmail.com>
2020-07-22 18:34:27 +0300

    Updates

---
+++ precache-manifest.d676ee11fde73d48fb106a7ed85719c2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1829dc950e1b799614753e6edc0a440",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7fbe37580d86b9921d4",
+    "url": "/static/css/main.6b95b1cc.chunk.css"
+  },
+  {
+    "revision": "d72bb05ddc8da9916f00",
+    "url": "/static/js/2.14c56a03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.14c56a03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7fbe37580d86b9921d4",
+    "url": "/static/js/main.a900fb0d.chunk.js"
+  },
+  {
+    "revision": "9faa06e49578191d5a13",
+    "url": "/static/js/runtime-main.66dd7950.js"
+  },
+  {
+    "revision": "9c3b03ae735a14dc757b4d25c0ba5fb3",
+    "url": "/static/media/Trash.9c3b03ae.svg"
+  },
+  {
+    "revision": "d476a8b86d0b656cfa463c1148e96069",
+    "url": "/static/media/avatar_edit.d476a8b8.svg"
+  },
+  {
+    "revision": "c2d0901f14de58acae456bda23f80e8a",
+    "url": "/static/media/crest.c2d0901f.svg"
+  },
+  {
+    "revision": "6c74d9b53442a247e20750f5de0be939",
+    "url": "/static/media/heart.6c74d9b5.svg"
+  },
+  {
+    "revision": "855a8c9865e79463f31e74f9abcb4438",
+    "url": "/static/media/logo.855a8c98.svg"
+  },
+  {
+    "revision": "c8252db2902c59bff2cb619de2b75c2f",
+    "url": "/static/media/pensil123.c8252db2.svg"
+  },
+  {
+    "revision": "bdfb8ebb113b9312e080c2f4b4213223",
+    "url": "/static/media/plus.bdfb8ebb.svg"
+  }
+]);
