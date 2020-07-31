001dd13baf233ae9758a15dafdd44e94aad1c90f
Marina Kuznetsova <marinaburykina@gmail.com>
2020-07-31 20:11:41 +0300

    bug

--- src/components/App.js
+++ src/components/App.js
@@ -8,8 +8,21 @@ import InformationPopup from "./InformationPopup.js";
 import AvatarPopup from "./AvatarPopup";
 import PictureAddPopup from "./PictureAddPopup";
 import ImagePopup from "./ImagePopup.js";
+import api from "../utils/Api";
+import {CurrentUserContext} from '../contexts/CurrentUserContext';
 
 function App() {
+    console.log('2');
+    const [currentUser, setCurrentUser] = React.useState();
+    React.useEffect(() => {
+        Promise.all([api.getUserInfo(), api.getInitialCards()])
+            .then(([user, cards]) => {
+                setCurrentUser(user);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
 
     const [dataImage, setDataImage] = React.useState({
         link: '',
@@ -19,6 +32,7 @@ function App() {
     const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(false);
     const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
     const [selectedCard, setSelectedCard] = React.useState(false);
+
 
     function closeAllPopups() {
         setIsEditAvatarPopupOpen(false);
@@ -46,21 +60,30 @@ function App() {
 
     return (
         <>
+
             <div className="page">
+                <CurrentUserContext.Provider value={currentUser}>
                 <Header/>
-                <Main onEditProfile={handleEditProfileClick} onAddPlace={handleAddPlaceClick}
-                      onEditAvatar={handleEditAvatarClick} onCardClick={handleCardClick}
-                />
+
+                    <Main onEditProfile={handleEditProfileClick} onAddPlace={handleAddPlaceClick}
+                          onEditAvatar={handleEditAvatarClick} onCardClick={handleCardClick}
+                    />
+
                 <Footer/>
-                <PopupWithForm name={'information'} title={'Редактировать профиль'} buttonText={'Сохранить'} children={<InformationPopup/>}
+                <PopupWithForm name={'information'} title={'Редактировать профиль'} buttonText={'Сохранить'}
+                               children={<InformationPopup/>}
                                isOpen={isEditProfilePopupOpen} close={closeAllPopups}/>
-                <PopupWithForm name={'picture-add'} title={'Новое место'} buttonText={'Создать'}  children={<PictureAddPopup/>}
+                <PopupWithForm name={'picture-add'} title={'Новое место'} buttonText={'Создать'}
+                               children={<PictureAddPopup/>}
                                isOpen={isAddPlacePopupOpen} close={closeAllPopups}/>
-                <PopupWithForm name={'avatar'} title={'Обновить аватар'} buttonText={'Сохранить'} children={<AvatarPopup/>}
+                <PopupWithForm name={'avatar'} title={'Обновить аватар'} buttonText={'Сохранить'}
+                               children={<AvatarPopup/>}
                                isOpen={isEditAvatarPopupOpen} close={closeAllPopups}/>
                 <PopupWithForm name={'sure'} title={'Вы уверены?'} buttonText={'Да'}/>
                 <ImagePopup isOpen={selectedCard} onClose={closeAllPopups} image={dataImage}/>
+            </CurrentUserContext.Provider>
             </div>
+
 
         </>
     );
